e864ccf0667e9990bab7c1db4a6098de68ca9ff4
Gareth Halfacree <gareth@halfacree.co.uk>
2012-09-17 21:33:48 +0100

    Initial script version.

---
+++ scantopdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Works around ImageMagick's stupid "NOMNOMNOM MEMORY" problem.
+
+for i in `ls *.png`
+do
+    echo Converting and resizing $i...
+    convert -resize 50% $i `echo $i | cut -d"." -f 1`.jpg
+done
+echo Conversion complete. Creating PDF at 300DPI...
+convert -density 300 *.jpg unoptimised.pdf
+echo PDF created. Optimising PDF...
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf unoptimised.pdf
+echo PDF optimised. Tidying temporary files...
+rm *.jpg unoptimised.pdf
+echo Finished. Final PDF file size: `ls -lh output.pdf | cut -d" " -f 5`
